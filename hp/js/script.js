060913a2eac88d0f3dd469ff8c463b2776a22c27
Uriol <uriol.bella@gmail.com>
2015-07-07 18:09:29 +0100

    updating hp

--- hp/js/script.js
+++ hp/js/script.js
@@ -1,9 +1,67 @@
+
+var intro_video = document.getElementById('intro_video'); 
+var width = window.innerWidth;
+var AR_height;
+
 $(function(){
 
 	resize();
 	function resize(){
+		width = window.innerWidth;
+		AR_height = width*9/16;
+		$('#img_intro').css('height', AR_height);
 
 	}
+
 	window.addEventListener( 'resize', resize, false );
 
+	
+
+	//playVideo();
+	// function playVideo(){
+	// 	intro_video.load();
+	// 	intro_video.play();
+
+	// }
+
+
+	// $('#intro_controls').on('click', function(){
+	// 	$('#intro_controls').css('background-image', 'none')
+	// 	if (intro_video.paused) {
+	// 		intro_video.play();
+	// 		$('#play_button').hide();
+	// 		$('#pause_button').show();
+	// 	} else {
+	// 		intro_video.pause();
+	// 		$('#play_button').show();
+	// 		$('#pause_button').hide();
+	// 	}
+	// })
+
+	// intro_video.addEventListener("ended", intro_ended);
+	// function intro_ended(){
+	// 	$('#intro_controls').css('background-image', 'url(img/bg_video.png)')
+	// }
+
+
+
+	// // Show controls on hover
+	// // $('.play_pause').hide();
+	// $('#intro_controls').on( "mouseenter", showControls );
+	// $('#intro_controls').on( "mouseleave", hideControls );
+	// function showControls(){
+	// 	$('.play_pause').show();
+	// 	if (intro_video.paused) {
+	// 		$('#play_button').show();
+	// 		$('#pause_button').hide();
+	// 	} else {
+	// 		$('#pause_button').show();
+	// 		$('#play_button').hide();
+	// 	}
+	// }
+	// function hideControls(){
+	// 	$('.play_pause').hide();
+	// 	$('#play_button').hide();
+	// 	$('#pause_button').hide();
+	// }
 })
